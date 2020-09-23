aba48f1293c3eb24a6e4a4719f0321e1d709a4a9
John Hoffer <john@hoff.in>
2020-09-22 21:23:29 -0400

    add marker and cell type links table to minerva-browser

--- index.js
+++ index.js
@@ -10,6 +10,5 @@ window.AuthenticationDetails = AmazonCognitoIdentity.AuthenticationDetails;
 import mb from 'minerva-browser'
 
 export default {
-  build_page: mb.build_page,
-  get_links_alias: mb.get_links_alias
+  build_page: mb.build_page
 }
