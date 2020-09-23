f4de108d117b15930939efb4f88ef46ebb3f5701
John Hoffer <john@hoff.in>
2020-09-23 10:55:51 -0400

    move more dependencies to minerva-browser

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-window.$ = window.jQuery = require('jquery');
-window.Popper = require('popper.js').default;
-require('bootstrap/dist/js/bootstrap');
-
 const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
 window.CognitoUser = AmazonCognitoIdentity.CognitoUser;
 window.CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
 window.AuthenticationDetails = AmazonCognitoIdentity.AuthenticationDetails;
 
 import mb from 'minerva-browser';
-window.OpenSeadragon = mb.OpenSeadragon;
 
 export default {
   build_page: mb.build_page
